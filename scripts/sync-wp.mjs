d6204c41c3f157a82ee92db896c9dd8c3eff4479
Vinay Thoke <reachme@vinaythoke.com>
2025-10-19 20:29:59 +0000

    Move sync script into Hugo repo for GitHub Actions

---
+++ scripts/sync-wp.mjs
@@ -0,0 +1,48 @@
+import fs from "fs";
+import fetch from "node-fetch";
+import path from "path";
+
+const query = `
+{
+  posts(first: 10, where: { status: PUBLISH }) {
+    nodes {
+      title
+      content
+      date
+      slug
+      featuredImage {
+	node {
+		sourceUrl
+	}
+      }
+    }
+  }
+}
+`;
+
+fetch("https://blog.digitalvin.com/graphql", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ query })
+})
+  .then(res => res.json())
+  .then(({ data }) => {
+    const outputDir = "digitalvin/content/post";
+    fs.mkdirSync(outputDir, { recursive: true });
+
+    data.posts.nodes.forEach(post => {
+
+const md = `---
+title: "${post.title}"
+date: ${post.date}
+slug: "${post.slug}"
+featured_image: "${post.featuredImage?.node?.sourceUrl || ''}"
+---
+
+${post.content}
+`;
+
+     fs.writeFileSync(`${outputDir}/${post.slug}.md`, md);
+    });
+  });
+
